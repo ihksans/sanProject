0e556841c3fedc18ad1d7b4e5436d52d0953ce73
ikhsan setiawan <ikhsan.setiawan.tif18@polban.ac.id>
2020-11-29 20:28:29 +0700

    Add Section 3

--- App.js
+++ App.js
@@ -13,9 +13,10 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
+  StatusBar,Button
 } from 'react-native';
-
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
 import {
   Header,
   LearnMoreLinks,
@@ -24,55 +25,38 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+
+export default class App extends React.Component
+ {
+   constructor(){
+     super();
+     this.checkPreviousSession();
+   }
+   async checkPreviousSession (){
+     const didCrash = await Crashes.hasCrashedInLastSession();
+     if(didCrash){
+       const report = await Crashes.lastSessionCrashReport();
+       alert("Sorry u has a that crash");
+     }
+   }
+   render(){
+     return (
+     <>
+     <View title="Crash">
+       <Button title="Calculate Information" onPress={() => Analytics.trackEvent('calculate_inflation')}>
+
+       </Button>
+      </View>
+     </>
+   );
+  }
+  
 };
 
 const styles = StyleSheet.create({
+  container:{
+    marginTop:40
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
   },
@@ -111,4 +95,3 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
